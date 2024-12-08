40eb44f47925bb1c25c2c6bfa4a3eafc3231b9d8
Miru <shedy.awsd@gmail.com>
2024-12-09 01:25:13 +0700

    login page prototype

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,28 @@
+'use client'
 import { title } from "@/components/primitives";
+import CoverImage from '@/assets/BannerLaboratorium.png';
+import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
 
 export default function LoginPage() {
   return (
-    <div>
-      <h1 className={title()}>Login</h1>
+    <div
+      className="w-full h-screen flex items-start"
+      style={{ backgroundImage: CoverImage.src, backgroundSize: 'cover', backgroundPosition: 'center' }}
+    >
+      <div className="relative w-full h-full flex-col">
+        {/* <img src={CoverImage.src} className="w-full h-full object-cover" /> */}
+      </div>
+      <Card
+        isBlurred
+        className="border-none dark:bg-white w-1/3 h-full"
+        shadow="sm"
+      >
+        <CardHeader className="h-1/4 flex flex-col justify-end">
+          <h1 className={title()} style={{ color: "black" }}>Login</h1>
+        </CardHeader>
+        <CardBody>
+        </CardBody>
+      </Card>
     </div>
   );
 }
