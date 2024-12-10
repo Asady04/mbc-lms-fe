3514a086b7e179428af9aaa9208d9c0dba9ccfaf
Miru <116446791+ShedMiru@users.noreply.github.com>
2024-12-10 08:00:31 +0700

    responsive login page n text prototype

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,26 +1,28 @@
 'use client'
-import { title } from "@/components/primitives";
+import { subtitle, title } from "@/components/primitives";
 import CoverImage from '@/assets/BannerLaboratorium.png';
 import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
 
 export default function LoginPage() {
   return (
     <div
-      className="w-full h-screen flex items-start"
-      style={{ backgroundImage: CoverImage.src, backgroundSize: 'cover', backgroundPosition: 'center' }}
+      className="max-w-full h-screen md:max-w-full flex items-start"
+    // style={{ backgroundImage: CoverImage.src, backgroundSize: 'cover', backgroundPosition: 'center' }}
     >
-      <div className="relative w-full h-full flex-col">
+      <div className="relative shrink-1 lg:w-full h-full flex-col">
         {/* <img src={CoverImage.src} className="w-full h-full object-cover" /> */}
       </div>
       <Card
-        isBlurred
-        className="border-none dark:bg-white w-1/3 h-full"
-        shadow="sm"
+        className="border-none rounded-sm dark:bg-white w-full lg:max-w-md lg:rounded-md h-full"
       >
-        <CardHeader className="h-1/4 flex flex-col justify-end">
-          <h1 className={title()} style={{ color: "black" }}>Login</h1>
+        <CardHeader className="h-1/2 flex flex-row justify-center">
+          <div className="inline-block max-w-xl text-center justify-center">
+            <h1 className={title()} style={{ color: "black" }}>Header</h1>
+            <h1 className={subtitle()} style={{ color: "black" }}>subtitle</h1>
+          </div>
         </CardHeader>
-        <CardBody>
+        <CardBody className="h-1/2 flex flex-row justify-center">
+          <h1 className={title()} style={{ color: "black" }}>Body</h1>
         </CardBody>
       </Card>
     </div>
